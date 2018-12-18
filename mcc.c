d7a882643dddbdaa44501613722282eefd751e8a
本川哲哉 <>
2018-12-18 23:16:53 +0900

    足し算と引き算を追加

--- mcc.c
+++ mcc.c
@@ -4,12 +4,33 @@
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "引数の個数が正しくありません\n");
+    return 1;
   }
+  char *p = argv[1];
 
   printf(".intel_syntax noprefix\n");
   printf(".globl _main\n");
   printf("_main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+  printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("  add rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    printf("  ret\n");
+    return 0;
+  }
+
+
   printf("  ret\n");
   return 0;
 }
